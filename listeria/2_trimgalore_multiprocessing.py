efdd5cf9afe6913a58c9f420673f4f32fc7ca609
Laura Carroll <lmc297@cornell.edu>
2024-10-17 10:43:20 +0200

    Create 2_trimgalore_multiprocessing.py

---
+++ listeria/2_trimgalore_multiprocessing.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys, os, glob
+import multiprocessing
+
+from multiprocessing.pool import ThreadPool
+
+def run_trimgalore(f1):
+  f2 = f1.replace("_R1.fastq.gz", "_R2.fastq.gz")
+  cmd = "trim_galore --cores 1 --fastqc --paired --gzip -o trimmed_reads {1} {2}".format(odir, f1, f2)
+  os.system(cmd)
+
+if __name__ == "__main__":
+  pool = multiprocessing.Pool(int(sys.argv[1]))
+  tasks = glob.glob("raw_reads/*_R1.fastq.gz")
+  pool.map(run_trimgalore, tasks)
