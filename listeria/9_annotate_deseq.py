58e1173464542235f5a349ad21b391c3b68f65ce
Laura Carroll <lmc297@cornell.edu>
2024-10-20 22:44:25 +0200

    Update 9_annotate_deseq.py

--- listeria/9_annotate_deseq.py
+++ listeria/9_annotate_deseq.py
@@ -3,8 +3,11 @@
 import sys, os, glob
 from Bio import SeqIO
 
+# add annotation information to DESeq results
+
+# create dictionary linking genes to eggnog annotations
 eggnog = {}
-with open("../eggnog/out.emapper.annotations", "r") as infile:
+with open("eggnog/out.emapper.annotations", "r") as infile:
 	for line in infile:
 		if not line.startswith("##"):
 			if "#query" in line:
@@ -13,9 +16,10 @@ with open("../eggnog/out.emapper.annotations", "r") as infile:
 				seqid = line.split("\t")[0].strip()
 				eggnog[seqid] = line.strip() 
 
+# create dictionary linking genes to NCBI annotations
 d = {}
 dlocus = {}
-with open("../eggnog/listeria_GCA_016306305.1.ffn", "r") as infile:
+with open("eggnog/listeria_GCA_016306305.1.ffn", "r") as infile:
 	for record in SeqIO.parse(infile, "fasta"):	
 		seqdes = str(record.description).strip()
 		seqid = str(record.id).strip()
@@ -24,7 +28,9 @@ with open("../eggnog/listeria_GCA_016306305.1.ffn", "r") as infile:
 		locus = locus.split("]")[0].strip()	
 		dlocus[locus] = seqid
 
-for f in glob.glob("../deseq_**.tsv"):
+# loop through DESeq results TSV files
+# add annotaation information and print TSV files
+for f in glob.glob("./deseq_**.tsv"):
 	fname = f.split("/")[-1].strip()
 	with open(f, "r") as infile:
 		for line in infile:
