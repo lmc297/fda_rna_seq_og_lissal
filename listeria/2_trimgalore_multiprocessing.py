9b1b164ce7778b248e199403ca5d1371ecd464ff
Laura Carroll <lmc297@cornell.edu>
2024-10-26 18:09:46 +0200

    Update 2_trimgalore_multiprocessing.py

--- listeria/2_trimgalore_multiprocessing.py
+++ listeria/2_trimgalore_multiprocessing.py
@@ -5,6 +5,8 @@ import multiprocessing
 
 from multiprocessing.pool import ThreadPool
 
+# run TrimGalore v0.6.10 on paired-end Illumina reads
+# the --stranded_illumina option was used (see https://github.com/FelixKrueger/TrimGalore/blob/master/Docs/Trim_Galore_User_Guide.md)  
 def run_trimgalore(f1):
   f2 = f1.replace("_R1.fastq.gz", "_R2.fastq.gz")
   cmd = "trim_galore --cores 1 --fastqc --paired --gzip --stranded_illumina -o trimmed_reads_stranded {0} {1}".format(f1, f2)
