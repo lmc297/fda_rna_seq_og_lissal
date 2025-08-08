035a41fbfafcac41b1b9a5c80e324e012fcd5a64
Laura Carroll <lmc297@cornell.edu>
2025-08-08 11:30:13 +0200

    Create 5_trimgalore_multiprocessing_stranded.py

---
+++ salmonella/5_trimgalore_multiprocessing_stranded.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys, os, glob
+import multiprocessing
+
+from multiprocessing.pool import ThreadPool
+
+def run_trimgalore(f1):
+  f2 = f1.replace("_R1.fastq.gz", "_R2.fastq.gz")
+  cmd = "trim_galore --cores 1 --fastqc --paired --gzip --stranded_illumina -o trimmed_reads_stranded {0} {1}".format(f1, f2)
+  os.system(cmd)
+
+if __name__ == "__main__":
+  pool = multiprocessing.Pool(int(sys.argv[1]))
+  tasks = glob.glob("trimmed_fastp/*_R1.fastq.gz")
+  pool.map(run_trimgalore, tasks)
